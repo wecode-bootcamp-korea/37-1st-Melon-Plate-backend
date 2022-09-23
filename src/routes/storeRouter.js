b79274c1532f9dfbfce4fde6173160c58699f701
es <mascoeskim@gmail.com>
2022-09-23 18:19:36 +0900

    error

--- src/routes/storeRouter.js
+++ src/routes/storeRouter.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 
 const { storeController } = require("../controllers");
 const { uploadFiles } = require("../middlewares");
-const { upload } = require("../middlewares/multer")
+const { upload } = require("../util/multer");
 const { accessToken } = require("../middlewares/auth");
 
 router
-  .route("/admin")
+  .route("/create")
   .post(upload.single("image"), accessToken, storeController.createStore);
 
 module.exports = router
