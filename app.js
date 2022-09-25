856feefae0e0b5af0c040fa1a33a303a6ce9cab5
es <mascoeskim@gmail.com>
2022-09-25 09:30:16 +0900

    table change

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(routes);
-// app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 
 app.get("/ping", (req, res, next) => {
   console.log(req.query)
