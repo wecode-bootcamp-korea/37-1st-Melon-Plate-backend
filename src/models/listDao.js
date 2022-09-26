75146a79811b5a2bd74dc4a65de30b0b8c27da8e
JUNHYUNG CHO <junhyungcho@JUNHYUNGui-MacBookPro.local>
2022-09-26 15:36:37 +0900

    맛집 리스트 기능 구현

--- src/models/listDao.js
+++ src/models/listDao.js
@@ -12,13 +12,16 @@ const getSeperatedList = async (
     stores.id,
     stores.address,
     stores.name,
-    stores.image
-    FROM stores, categories, menus, reviews
-    WHERE categories.id = stores.category_id AND reviews.store_id=stores.id AND menus.store_id = stores.id
-    AND stores.address LIKE ("%"?"%")
-    AND categories.category LIKE ("%"?"%")
-    AND menus.name LIKE ("%"?"%")
-    LIMIT ?
+    stores.image,
+    FORMAT(AVG(reviews.rate),2) AS rate_average
+   FROM stores, categories, menus, reviews
+   WHERE categories.id = stores.category_id AND reviews.store_id=stores.id AND menus.store_id = stores.id
+   AND stores.address LIKE ("%"?"%")
+   AND categories.category LIKE ("%"?"%")
+   AND menus.name LIKE ("%"?"%")
+   GROUP BY stores.id 
+   ORDER BY rate_average desc
+   LIMIT ?
 	`,
     [ address, category, menu, limit ]
   );
