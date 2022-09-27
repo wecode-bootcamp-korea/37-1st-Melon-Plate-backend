a9fa5c2b2bcd78c2c075e4996de38643685ec7a1
JUNHYUNG CHO <junhyungcho@JUNHYUNGui-MacBookPro.local>
2022-09-28 00:22:11 +0900

    wip

--- src/models/mainDao.js
+++ src/models/mainDao.js
@@ -1,7 +1,9 @@
 const database = require("./dataSource");
 
 const getSearchResult = async (query, filter, price, location, category) => {
-  console.log(query, filter, price, location, category);
+  const priceRange = (price) ? (`stores.price_range between ${price} and 5 AND`) : "";
+  const orderBy = (filter) ? (`ORDER BY ${filter} desc`) : "";
+
   const result = await database.query(
     `
    SELECT DISTINCT
@@ -21,21 +23,21 @@ const getSearchResult = async (query, filter, price, location, category) => {
     ON stores.id = reviews.store_id
    INNER JOIN menus
     ON menus.store_id = stores.id
-   WHERE stores.address LIKE ("%"?"%")
+   WHERE ${priceRange} stores.address LIKE ("%"?"%")
     OR stores.name LIKE ("%"?"%") 
     OR menus.store_id = stores.id AND menus.name LIKE ("%"?"%")
     OR categories.category LIKE ("%"?"%")
-    AND stores.address LIKE ("%"?"%")
-    AND categories.category LIKE ("%"?"%")
+   AND stores.address LIKE ("%"?"%")
+   AND categories.category LIKE ("%"?"%")
    GROUP BY stores.id
-   ORDER BY ${filter} desc
-      `,
-    [query, query, query, query, location, category]
-  );
-
-  return result;
-};
-
+   ${orderBy}
+   `,
+   [query, query, query, query, location, category]
+   );
+   
+   return result;
+  };
+  
 module.exports = {
   getSearchResult,
 };
